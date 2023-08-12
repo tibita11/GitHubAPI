b07914ecf2410270d4b7f5ab5eb05c935b4bde52
tibita11 <tibita0811@gmail.com>
2023-08-12 16:43:04 +0900

    feat: 初期画面作成

---
+++ GitHubAPI/Functions/Common/MainTabBarController.swift
@@ -0,0 +1,24 @@
+//
+//  MainTabBarController.swift
+//  GitHubAPI
+//
+//  Created by 鈴木楓香 on 2023/08/12.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpViewController()
+    }
+    
+    private func setUpViewController() {
+        let searchVC = SearchViewController()
+        searchVC.tabBarItem.title = "Search"
+        searchVC.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        viewControllers = [searchVC]
+    }
+}
---
+++ GitHubAPI/Functions/Search/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  GitHubAPI
+//
+//  Created by 鈴木楓香 on 2023/08/12.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemBackground
+    }
+}
